28d0ff324eefbc4f493a042cae9be1d6cd781a9d
Michaël Zasso <targos@protonmail.com>
2016-07-08 16:11:43 +0200

    feat: initial version

---
+++ src/index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const jcampconverter = require('jcampconverter');
+
+const getSpectrumType = require('./getSpectrumType');
+
+/**
+ * @typedef NMRMetadata
+ * @type Object
+ * @property {number} dimension
+ * @property {number[]} nucleus
+ * @property {string} title
+ * @property {string} solvent
+ * @property {string} pulse
+ * @property {string} experiment
+ * @property {number} temperature - Temperature in K
+ * @property {number} frequency
+ * @property {string} date - Date in ISO string format
+ */
+
+/**
+ * Returns a metadata object from JCAMP
+ * @param {string} jcampData
+ * @return {NMRMetadata} metadata
+ */
+exports.parseJcamp = function (jcampData) {
+    const jcamp = jcampconverter.convert(jcampData.toString(), {
+        keepRecordsRegExp: /.*/,
+        withoutXY: true
+    });
+
+    const metadata = {
+        dimension: jcamp.twoD ? 2 : 1,
+        nucleus: []
+    };
+
+    const info = jcamp.info;
+    maybeAdd(metadata, 'title', info['TITLE']);
+    maybeAdd(metadata, 'solvent', info['.SOLVENTNAME']);
+    maybeAdd(metadata, 'pulse', info['.PULSESEQUENCE'] || info['.PULPROG'] || info['$PULPROG']);
+    maybeAdd(metadata, 'experiment', getSpectrumType(metadata.pulse));
+    maybeAdd(metadata, 'temperature', parseFloat(info['$TE'] || info['.TE']));
+    maybeAdd(metadata, 'frequency', parseFloat(info['.OBSERVEFREQUENCY']));
+
+    if (metadata.dimension === 1) {
+        const nucleus = info['.OBSERVENUCLEUS'];
+        if (nucleus) {
+            metadata.nucleus = [nucleus.replace(/[^A-Za-z0-9]/g,'')];
+        }
+    } else {
+        const nucleus = info['.NUCLEUS'];
+        if (nucleus) {
+            metadata.nucleus = nucleus.split(',').map(nuc => nuc.trim());
+        }
+    }
+    if (metadata.nucleus.length === 0) {
+        metadata.nucleus = exports.getNucleusFrom2DExperiment(metadata.experiment);
+    }
+
+    if (info['$DATE']) {
+        metadata.date = (new Date(info['$DATE'] * 1000)).toISOString();
+    }
+
+    return metadata;
+};
+
+/**
+ * Returns a list of nuclei based on an experiment string
+ * @param {string} experiment
+ * @return {string[]}
+ */
+exports.getNucleusFrom2DExperiment = function (experiment) {
+    if (typeof experiment !== 'string') {
+        return [];
+    }
+    experiment = experiment.toLowerCase();
+    if (experiment.includes('jres')) {
+        return ['1H'];
+    }
+    if (experiment.includes('hmbc') || experiment.includes('hsqc')) {
+        return ['1H', '13C'];
+    }
+    return ['1H', '1H'];
+};
+
+exports.getSpectrumType = getSpectrumType;
+
+function maybeAdd(obj, name, value) {
+    if (value) {
+        obj[name] = value;
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const nmrMetadata = require('..');
+
+describe('getSpectrumType', function () {
+    it('should return empty string for unknown pulse', function () {
+        check([
+            ['', ''],
+            ['aaa', ''],
+            [null, ''],
+            ['test', '']
+        ]);
+    });
+    it('should know about real pulse sequences', function () {
+        check([
+            ['zg30', '1d'],
+            ['zghfigqn', '1d'],
+
+            ['hsqct1etf3gpsi', 'hsqctocsy'],
+
+            ['hsqcedetgpsisp2.3', 'hsqc'],
+            ['hsqcdietgpiasisp', 'hsqc'],
+
+            ['hmbcgplpndqf', 'hmbc'],
+            ['shmbcgpndqf', 'hmbc'],
+            ['hmbcacbigpl2ndqf', 'hmbc'],
+
+            ['cosygpppqf', 'cosy'],
+            ['cosycwphps', 'cosy'],
+            ['cosydfetgp.1', 'cosy'],
+
+            ['hjresqf', 'jres'],
+            ['lcjrescwfdprqf', 'jres'],
+
+            ['atocsygpph19', 'tocsy'],
+            ['dipsi2esfbgpph', 'tocsy'],
+            ['mlevesgpph', 'tocsy'],
+
+            ['noesygpphwgxf', 'noesy'],
+            ['stdnoesyesgpph', 'noesy'],
+
+            ['roesyesgpph', 'roesy'],
+            ['troesyphpr', 'roesy'],
+
+            ['deptppnd', 'dept'],
+            ['hxdeptbiph', 'dept'],
+
+            ['jmod', 'aptjmod'],
+            ['apt', 'aptjmod']
+        ]);
+    });
+});
+
+function check(arr) {
+    for (var test of arr) {
+        nmrMetadata.getSpectrumType(test[0]).should.equal(test[1]);
+    }
+}
