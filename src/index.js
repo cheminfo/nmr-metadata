d2f4235778c57d59584e47a4f7a7e8383fcea716
Luc Patiny <luc@patiny.com>
2018-08-21 14:58:05 +0200

    add expno in metadata

--- src/index.js
+++ src/index.js
@@ -17,20 +17,21 @@ const getSpectrumType = require('./getSpectrumType');
  * @property {string} experiment
  * @property {number} temperature - Temperature in Kelvin
  * @property {number} frequency
+ * @property {number} expno - Experience number
  * @property {string} date - Date in ISO string format
  * @property {object} ranges
  */
 
 const defaultOptions = {
-    computeRanges: false
+  computeRanges: false
 };
 
 const defaultRangesOptions = {
-    nH: 100,
-    thresholdFactor: 0.85,
-    clean: 0.4,
-    compile: true,
-    integralType: 'sum'
+  nH: 100,
+  thresholdFactor: 0.85,
+  clean: 0.4,
+  compile: true,
+  integralType: 'sum'
 };
 
 /**
@@ -41,74 +42,88 @@ const defaultRangesOptions = {
  * @param {number} [options.ranges] - options for ranges computation
  * @return {NMRMetadata} metadata
  */
-exports.parseJcamp = function (jcampData, options) {
-    options = Object.assign({}, defaultOptions, options);
+exports.parseJcamp = function(jcampData, options) {
+  options = Object.assign({}, defaultOptions, options);
 
-    const jcampString = jcampData.toString();
-    const jcamp = jcampconverter.convert(jcampString, {
-        keepRecordsRegExp: /.*/,
-        withoutXY: true
+  const jcampString = jcampData.toString();
+  const jcamp = jcampconverter.convert(jcampString, {
+    keepRecordsRegExp: /.*/,
+    withoutXY: true
+  });
+
+  const metadata = {
+    dimension: jcamp.twoD ? 2 : 1,
+    nucleus: [],
+    isFid: false,
+    isFt: false
+  };
+
+  const info = jcamp.info;
+  maybeAdd(metadata, 'title', info['TITLE']);
+  maybeAdd(metadata, 'solvent', info['.SOLVENTNAME']);
+  maybeAdd(
+    metadata,
+    'pulse',
+    info['.PULSESEQUENCE'] || info['.PULPROG'] || info['$PULPROG']
+  );
+  maybeAdd(metadata, 'experiment', getSpectrumType(metadata.pulse));
+  maybeAdd(metadata, 'temperature', parseFloat(info['$TE'] || info['.TE']));
+  maybeAdd(metadata, 'frequency', parseFloat(info['.OBSERVEFREQUENCY']));
+  maybeAdd(metadata, 'type', info['DATATYPE']);
+  maybeAdd(metadata, 'expno', parseInt(info['$EXPNO']));
+  if (metadata.type) {
+    if (metadata.type.toUpperCase().indexOf('FID') >= 0) metadata.isFid = true;
+    else if (metadata.type.toUpperCase().indexOf('SPECTRUM') >= 0) {
+      metadata.isFt = true;
+    }
+  }
+
+  if (metadata.dimension === 1) {
+    const nucleus = info['.OBSERVENUCLEUS'];
+    if (nucleus) {
+      metadata.nucleus = [nucleus.replace(/[^A-Za-z0-9]/g, '')];
+    }
+  } else {
+    const nucleus = info['.NUCLEUS'];
+    if (nucleus) {
+      metadata.nucleus = nucleus.split(',').map(nuc => nuc.trim());
+    }
+  }
+  if (metadata.nucleus.length === 0) {
+    metadata.nucleus = exports.getNucleusFrom2DExperiment(metadata.experiment);
+  }
+
+  if (info['$DATE']) {
+    metadata.date = new Date(info['$DATE'] * 1000).toISOString();
+  }
+
+  if (
+    options.computeRanges &&
+    metadata.isFt &&
+    metadata.dimension === 1 &&
+    metadata.nucleus[0] === '1H'
+  ) {
+    const rangesOptions = Object.assign(
+      {},
+      defaultRangesOptions,
+      options.ranges
+    );
+    if (options.removeImpurities && metadata.solvent)
+      rangesOptions.removeImpurity = { solvent: metadata.solvent };
+    const spectrum = SD.NMR.fromJcamp(jcampString);
+    const ranges = spectrum.getRanges(rangesOptions);
+    ranges.forEach(function(range) {
+      // todo remove when there is an option to avoid that
+      delete range._highlight;
+      delete range.signalID;
+      range.signal.forEach(function(signal) {
+        delete signal.peak;
+      });
     });
+    metadata.range = ranges;
+  }
 
-    const metadata = {
-        dimension: jcamp.twoD ? 2 : 1,
-        nucleus: [],
-        isFid: false,
-        isFt: false
-    };
-
-    const info = jcamp.info;
-    maybeAdd(metadata, 'title', info['TITLE']);
-    maybeAdd(metadata, 'solvent', info['.SOLVENTNAME']);
-    maybeAdd(metadata, 'pulse', info['.PULSESEQUENCE'] || info['.PULPROG'] || info['$PULPROG']);
-    maybeAdd(metadata, 'experiment', getSpectrumType(metadata.pulse));
-    maybeAdd(metadata, 'temperature', parseFloat(info['$TE'] || info['.TE']));
-    maybeAdd(metadata, 'frequency', parseFloat(info['.OBSERVEFREQUENCY']));
-    maybeAdd(metadata, 'type', info['DATATYPE']);
-
-    if(metadata.type){
-        if(metadata.type.toUpperCase().indexOf('FID') >= 0)
-            metadata.isFid = true;
-        else if(metadata.type.toUpperCase().indexOf('SPECTRUM') >= 0 ){
-            metadata.isFt = true;
-        }
-    }
-
-    if (metadata.dimension === 1) {
-        const nucleus = info['.OBSERVENUCLEUS'];
-        if (nucleus) {
-            metadata.nucleus = [nucleus.replace(/[^A-Za-z0-9]/g,'')];
-        }
-    } else {
-        const nucleus = info['.NUCLEUS'];
-        if (nucleus) {
-            metadata.nucleus = nucleus.split(',').map(nuc => nuc.trim());
-        }
-    }
-    if (metadata.nucleus.length === 0) {
-        metadata.nucleus = exports.getNucleusFrom2DExperiment(metadata.experiment);
-    }
-
-    if (info['$DATE']) {
-        metadata.date = (new Date(info['$DATE'] * 1000)).toISOString();
-    }
-
-    if (options.computeRanges && metadata.isFt && metadata.dimension === 1 && metadata.nucleus[0] === '1H') {
-        const rangesOptions = Object.assign({}, defaultRangesOptions, options.ranges);
-        if (options.removeImpurities && metadata.solvent) rangesOptions.removeImpurity = {solvent: metadata.solvent};
-        const spectrum = SD.NMR.fromJcamp(jcampString);
-        const ranges = spectrum.getRanges(rangesOptions);
-        ranges.forEach(function (range) { // todo remove when there is an option to avoid that
-            delete range._highlight;
-            delete range.signalID;
-            range.signal.forEach(function (signal) {
-                delete signal.peak;
-            });
-        });
-        metadata.range = ranges;
-    }
-
-    return metadata;
+  return metadata;
 };
 
 /**
@@ -116,24 +131,24 @@ exports.parseJcamp = function (jcampData, options) {
  * @param {string} experiment
  * @return {string[]}
  */
-exports.getNucleusFrom2DExperiment = function (experiment) {
-    if (typeof experiment !== 'string') {
-        return [];
-    }
-    experiment = experiment.toLowerCase();
-    if (experiment.includes('jres')) {
-        return ['1H'];
-    }
-    if (experiment.includes('hmbc') || experiment.includes('hsqc')) {
-        return ['1H', '13C'];
-    }
-    return ['1H', '1H'];
+exports.getNucleusFrom2DExperiment = function(experiment) {
+  if (typeof experiment !== 'string') {
+    return [];
+  }
+  experiment = experiment.toLowerCase();
+  if (experiment.includes('jres')) {
+    return ['1H'];
+  }
+  if (experiment.includes('hmbc') || experiment.includes('hsqc')) {
+    return ['1H', '13C'];
+  }
+  return ['1H', '1H'];
 };
 
 exports.getSpectrumType = getSpectrumType;
 
 function maybeAdd(obj, name, value) {
-    if (value) {
-        obj[name] = value;
-    }
+  if (value) {
+    obj[name] = value;
+  }
 }
--- test/testDx.js
+++ test/testDx.js
@@ -4,54 +4,69 @@ const nmrMetadata = require('..');
 const FS = require('fs');
 
 function read(file) {
-    return FS.readFileSync(__dirname + '/data/' + file, 'utf8');
+  return FS.readFileSync(__dirname + '/data/' + file, 'utf8');
 }
 
-describe('getType', function () {
-    it('should be FID', function () {
-        const metadata = nmrMetadata.parseJcamp(read('fid.dx'));
-        metadata['type'].should.eql('NMR FID');
-        metadata.isFid.should.eql(true);
-        metadata.isFt.should.eql(false);
+describe('getType', function() {
+  it('should be FID', function() {
+    const metadata = nmrMetadata.parseJcamp(read('fid.dx'));
+    metadata['type'].should.eql('NMR FID');
+    metadata.isFid.should.eql(true);
+    metadata.isFt.should.eql(false);
+  });
+
+  it('should be FT', function() {
+    const metadata = nmrMetadata.parseJcamp(read('ft.dx'));
+    metadata['type'].should.eql('NMR SPECTRUM');
+    metadata.isFid.should.eql(false);
+    metadata.isFt.should.eql(true);
+  });
+
+  it('should parse test2 without infinite loop', function() {
+    const metadata = nmrMetadata.parseJcamp(read('test2.jdx'), {
+      computeRanges: true
     });
+    metadata['type'].should.eql('NMR SPECTRUM');
+    metadata.isFid.should.eql(false);
+    metadata.isFt.should.eql(true);
+    metadata.expno.should.eql(1);
+  });
 
-    it('should be FT', function () {
-        const metadata = nmrMetadata.parseJcamp(read('ft.dx'));
-        metadata['type'].should.eql('NMR SPECTRUM');
-        metadata.isFid.should.eql(false);
-        metadata.isFt.should.eql(true);
+  it('should parse test3 without infinite loop', function() {
+    const metadata = nmrMetadata.parseJcamp(read('test3.jdx'), {
+      computeRanges: true
     });
+    metadata['type'].should.eql('NMR SPECTRUM');
+    metadata.isFid.should.eql(false);
+    metadata.isFt.should.eql(true);
+  });
 
-    it('should parse test2 without infinite loop', function () {
-        const metadata = nmrMetadata.parseJcamp(read('test2.jdx'), {computeRanges: true});
-        metadata['type'].should.eql('NMR SPECTRUM');
-        metadata.isFid.should.eql(false);
-        metadata.isFt.should.eql(true);
+  it('should compute ranges', function() {
+    const metadata = nmrMetadata.parseJcamp(read('ft-data.jdx'), {
+      computeRanges: true,
+      ranges: { nH: 10 }
     });
+    metadata.range.should.be.an.Array().with.a.lengthOf(4);
+    metadata.range[0].should.have.properties([
+      'from',
+      'to',
+      'integral',
+      'signal'
+    ]);
+    metadata.range[0].signal.should.be.an.Array();
+    metadata.range[0].signal[0].should.have.properties([
+      'delta',
+      'diaID',
+      'multiplicity'
+    ]);
+  });
 
-    it('should parse test3 without infinite loop', function () {
-        const metadata = nmrMetadata.parseJcamp(read('test3.jdx'), {computeRanges: true});
-        metadata['type'].should.eql('NMR SPECTRUM');
-        metadata.isFid.should.eql(false);
-        metadata.isFt.should.eql(true);
-    });
-
-
-    it('should compute ranges', function () {
-        const metadata = nmrMetadata.parseJcamp(read('ft-data.jdx'), {computeRanges: true, ranges: {nH: 10}});
-        metadata.range.should.be.an.Array().with.a.lengthOf(4);
-        metadata.range[0].should.have.properties(['from', 'to', 'integral', 'signal']);
-        metadata.range[0].signal.should.be.an.Array();
-        metadata.range[0].signal[0].should.have.properties(['delta', 'diaID', 'multiplicity']);
-    });
-
-    it('should be mestrec', function () {
-        const metadata = nmrMetadata.parseJcamp(read('mestrec.jcamp'));
-        metadata.type.should.eql('NMRSPECTRUM');
-        metadata.isFid.should.eql(false);
-        metadata.isFt.should.eql(true);
-        metadata.solvent.should.eql('MeOD');
-        metadata.frequency.should.eql( 400.1318406);
-    });
-
+  it('should be mestrec', function() {
+    const metadata = nmrMetadata.parseJcamp(read('mestrec.jcamp'));
+    metadata.type.should.eql('NMRSPECTRUM');
+    metadata.isFid.should.eql(false);
+    metadata.isFt.should.eql(true);
+    metadata.solvent.should.eql('MeOD');
+    metadata.frequency.should.eql(400.1318406);
+  });
 });
